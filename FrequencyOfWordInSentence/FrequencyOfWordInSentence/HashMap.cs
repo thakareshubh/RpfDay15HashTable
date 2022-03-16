33609c28f3e877e6533c789a2e440c21df3be18a
thakareshubh <shubhamthakare032@gmail.com>
2022-03-16 12:45:22 +0530

    Check frequency of word in sentence

---
+++ FrequencyOfWordInSentence/FrequencyOfWordInSentence/HashMap.cs
@@ -0,0 +1,106 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FrequencyOfWordInSentence
+{
+    public class HashMap<K, V>
+    {        
+        /// <summary>
+        /// The size and key value pair  are initializig.
+        /// </summary>
+        private readonly int size;
+        private readonly LinkedList<KeyValue<K, V>>[] items;
+        
+
+        /// <summary>
+        /// Initializes a new instance of the <see cref="HashMap{K, V}"/> class.
+        /// </summary>
+        /// <param name="size">The size.</param>
+       
+        public HashMap(int size)
+        {
+            this.size = size;
+            this.items = new LinkedList<KeyValue<K, V>>[size];
+        }
+        
+
+         /// <summary>
+         /// Gets the linked list.
+         /// </summary>
+         /// <param name="position">The position.</param>
+         /// <returns></returns>
+         protected LinkedList<KeyValue<K, V>> GetLinkedList(int position)
+        {
+            LinkedList<KeyValue<K, V>> linkList = items[position];
+            if (linkList == null)
+            {
+                linkList = new LinkedList<KeyValue<K, V>>();
+                items[position] = linkList;
+            }
+            return linkList;
+        }
+        
+
+         /// <summary>
+         /// Gets the array position.
+         /// </summary>
+         /// <param name="key">The key.</param>
+         /// <returns></returns>
+         protected int GetArrayPosition(K key)
+        {
+            int position = key.GetHashCode() % size;
+            return Math.Abs(position);
+        }
+        
+
+         /// <summary>
+         /// Gets the specified key.
+         /// </summary>
+         /// <param name="key">The key.</param>
+         /// <returns></returns>
+         public V Get(K key)
+        {
+            int position = GetArrayPosition(key);
+            LinkedList<KeyValue<K, V>> linkedlist = GetLinkedList(position);
+            foreach (KeyValue<K, V> item in linkedlist)
+            {
+                if (item.Key.Equals(key) )
+                {
+                    return item.Value;
+                }
+
+            }
+            return default(V);
+        }
+        
+
+        /// <summary>
+        /// Adds the specified key.
+        /// </summary>
+        /// <param name="key">The key.</param>
+        /// <param name="value">The value.</param>
+        public void Add(K key, V value)
+        {
+            int position = GetArrayPosition(key);
+            LinkedList<KeyValue<K, V>> linklst = GetLinkedList(position);
+            KeyValue<K, V> item = new KeyValue<K, V>() { Key = key, Value = value };
+            linklst.AddLast(item);
+        }
+
+
+
+    }     
+/// <summary>
+/// Assing the value
+/// </summary>
+/// <typeparam name="k"></typeparam>
+/// <typeparam name="v"></typeparam>
+     public struct KeyValue<k, v>
+        {
+            public k Key { get; set; }
+            public v Value { get; set; }
+        }
+}
---
+++ FrequencyOfWordInSentence/FrequencyOfWordInSentence/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+namespace FrequencyOfWordInSentence
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            
+            
+            Console.WriteLine("Hash table demo");
+            HashMap<string, string> hash = new HashMap<string, string>(5);
+            
+            hash.Add("0", "To");
+            hash.Add("1", "be");
+            hash.Add("2", "or");
+            hash.Add("3", "not");
+            hash.Add("4", "to");
+            hash.Add("5", "be");
+
+            string hash5 = hash.Get("5");
+            string hash2 = hash.Get("2");
+            string hash3 = hash.Get("3");
+            string hash1 = hash.Get("1");
+            Console.WriteLine("5th index value: " + hash5);
+        }
+    }
+}
