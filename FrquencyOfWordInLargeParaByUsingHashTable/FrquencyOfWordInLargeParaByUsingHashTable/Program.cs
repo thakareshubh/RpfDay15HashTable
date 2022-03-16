89fde2947817399199b3cc5dabe2a23a19e5f0cd
thakareshubh <shubhamthakare032@gmail.com>
2022-03-16 13:03:46 +0530

    Added code to find word from para

---
+++ FrquencyOfWordInLargeParaByUsingHashTable/FrquencyOfWordInLargeParaByUsingHashTable/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+
+namespace FrquencyOfWordInLargeParaByUsingHashTable
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            // creating object of hashtable class
+            
+            HashTableList<string, string> hash = new HashTableList<string, string>(13);
+            
+            //storing the value at key point
+
+            hash.Add("0", "Paranoids");
+            hash.Add("1", "are");
+            hash.Add("2", "not");
+            hash.Add("3", "Paranoid");
+            hash.Add("4", "because");
+            hash.Add("5", "they");
+            hash.Add("6", "keep");
+            hash.Add("7", "putting");
+            hash.Add("8", "themselves");
+            hash.Add("9", "deliberatly");
+            hash.Add("10", "into");
+            hash.Add("11", "avoidable");
+            hash.Add("12", "situation");
+
+            ///calling the value by usng key value pair
+
+            string hash7 = hash.Get("7");
+            Console.WriteLine("7 th index value: " + hash7);
+
+
+
+
+        }
+    }
+}
